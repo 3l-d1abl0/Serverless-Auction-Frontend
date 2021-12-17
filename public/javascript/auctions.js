597fd64469f5d72bf5986c2cc662fa772fd176eb
sameer <sameer.barha12@gmail.com>
2021-12-17 14:28:38 +0530

    auction page countdown working

--- public/javascript/auctions.js
+++ public/javascript/auctions.js
@@ -1,5 +1,37 @@
-$(document).ready({
+$(document).ready(function() {
 
-    console.log($(".auctionCardFoot-details-header").length());
+    function initializeClock(element, endingDate){
+
+        function updateClock() {
+
+            let deadline = new Date(endingDate) - new Date();
+            console.log(deadline);
+
+            if (deadline <= 0 || deadline === NaN) {
+                clearInterval(timeinterval);
+            }
+
+            const seconds = Math.floor((deadline / 1000) % 60);
+            const minutes = Math.floor((deadline / (1000 * 60) ) % 60);
+            const hours = Math.floor((deadline / (1000 * 60 * 60)) % 60);
+
+            let text = `${hours} hours ${minutes} minutes ${seconds} seconds`;
+            $(element).text(text);
+            console.log(element, text);
+
+        }
+        
+        updateClock();
+        const timeinterval = setInterval(updateClock, 1000);
+ 
+    }
+    
+    $('.auctionCardFoot-details-header').map(function(item, index){
+        let endingDate = $(index).data("endingat");
+    
+        if( endingDate !== undefined){
+            initializeClock(index, endingDate);
+        }
+    })
 
 });
