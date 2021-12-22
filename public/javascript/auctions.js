0df156a4c09ecf81d9e1235ea4c5b8a55096fde7
sameer <sameer.barha12@gmail.com>
2021-12-23 00:08:08 +0530

    Finished Auction Page

--- public/javascript/auctions.js
+++ public/javascript/auctions.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {
 
+    $(".backdrop").click(function(){
+        console.log('clicked');
+        $(".backdrop").hide();
+        $(".modal").hide();
+    });
+
     function initializeClock(element, endingDate){
 
         function updateClock() {
 
             let deadline = new Date(endingDate) - new Date();
-            console.log(deadline);
 
-            if (deadline <= 0 || deadline === NaN) {
+            if ( deadline <= 0 || isNaN(deadline) ) {
                 clearInterval(timeinterval);
             }
 
             const seconds = Math.floor((deadline / 1000) % 60);
             const minutes = Math.floor((deadline / (1000 * 60) ) % 60);
-            const hours = Math.floor((deadline / (1000 * 60 * 60)) % 60);
+            const hours = Math.floor((deadline / (1000 * 60 * 60)));
 
-            let text = `${hours} hours ${minutes} minutes ${seconds} seconds`;
+            let text = `${hours} hrs ${minutes} min ${seconds} sec`;
             $(element).text(text);
-            console.log(element, text);
 
         }
         
         updateClock();
-        const timeinterval = setInterval(updateClock, 1000);
+        var timeinterval = setInterval(updateClock, 1000);
  
     }
     
@@ -32,6 +36,119 @@ $(document).ready(function() {
         if( endingDate !== undefined){
             initializeClock(index, endingDate);
         }
-    })
+    });
 
+    $('.auctionCardFooter-button-bid').click(function(){
+
+         let auctionid = $(this).data("auctionid");
+
+         let auctionNameEle = $(".auctionCardHead-avatar-name").filter(function(index, item){
+            return $(item).data("auctionid")== auctionid;
+         });
+
+         let auctionPriceEle = $(".auctionCardFoot-details-header").filter(function(index, item){
+            return $(item).data("auctionid")== auctionid;
+         });
+
+         $(".modal h2").text( $(auctionNameEle).text() );
+
+         $(".modal .form-input-wrapper .form-input").val( parseInt($(auctionPriceEle).text().trim())+1 );
+
+         $(".modal .form-input-wrapper .form-input").data("auctionid", auctionid);
+
+         $(".backdrop").show(); $(".modal").show();
+
+    });
+
+
+    $('.confirm-button').click(function(){
+
+        let auctionId = $(".modal .form-input-wrapper .form-input").data("auctionid");
+        let auctionBidPrice = $(".modal .form-input-wrapper .form-input").val();
+
+        let message = $('div.modal-message');
+        message.text('');
+
+        $.ajax({ 
+            url: `/auction/${auctionId}/bid`,
+            type: 'POST',
+            data: { auctionId: auctionId, auctionBidPrice : auctionBidPrice },
+            success: function(result) {
+
+                    message.text('PLACED !');
+
+                   let targetEle = $('.auctionCardHead-avatar-name').filter(function(index, item){
+                        return $(item).data("auctionid") == auctionId;
+                   });
+
+                   let parentEle = targetEle.closest('.auctionsCardWrapper');
+
+                   let newHtml = `<button class="auctionCardHeader">
+
+                   <div class="auctionCardHead">
+                       <div class="auctionCardHead-avatar">
+                           <div class="auctionCardHead-avatar-image">
+                               ${result.data.seller[0].toUpperCase()}
+                           </div>
+                       </div>
+                       <div class="auctionCardHead-content">
+                           <span class="auctionCardHead-avatar-name" data-auctionId="${result.data.id}">
+                               ${result.data.title}
+                           </span>
+                       </div>
+                   </div>
+
+                   <div class="auctionCardBody">
+                   </div>
+
+                   <div class="auctionCardFoot">
+                       <div class="auctionCardFoot-details">
+                           <div>
+                               <span class="auctionCardFoot-details-header" data-auctionId="${result.data.id}">
+                                    ${result.data.highestBid.amount}
+                               </span>
+                               <p class="auctionCardFoot-details-data">
+                                   HIGHEST BID
+                               </p>
+                           </div>
+                       </div>
+                       <div class="auctionCardFoot-details">
+                           <div>
+                               <span class="auctionCardFoot-details-header auction-timer" data-endingAt="${result.data.endingAt}" >
+                                   -- hours -- mins -- secs
+                               </span>
+                               <p class="auctionCardFoot-details-data">
+                                   TIME REMAINING
+                               </p>
+                           </div>
+                       </div>
+                   </div>
+
+               </button>
+
+               <div class="auctionCardFooter">
+
+                   <div class="auctionCardFooter-card">
+
+                           <button class="auctionCardFooter-button">
+                               <span class="auctionCardFooter-label">You are the Highest Bidder !</span>
+                           </button>
+
+                   </div>
+                       
+
+               </div>`;
+
+               parentEle.html(newHtml);
+               initializeClock($('.auctionCardFoot-details-header.auction-timer'), result.data.endingAt);
+
+               $(".backdrop").hide(); $(".modal").hide();
+
+            },
+            error: function(error) {
+                message.text(error.responseJSON.data);
+            }
+        });
+
+    });
 });
--- routes/auctions.js
+++ routes/auctions.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const logger = require('../config/logger');
+var secured = require('../lib/middleware/secured');
 
 /* GET user profile. */
-router.get('/', function (req, res, next) {
-  
+router.get('/', secured(), function (req, res, next) {
+
+    //If no user logged in , take it to login Page
+    if(req.user == undefined){
+      return res.redirect('/');
+    }
+    
     //Get available auctions
+    let getAuctionsUrl = process.env.AUTH0_APP_AUCTIONS_ENDPOINT+"/auctions?status=OPEN";
+    axios.get(
+        getAuctionsUrl,
+        {   headers: {
+                "Authorization": "Bearer "+req.session.id_token
+            },
+            redirect: 'follow'
+        }).then((response) =>{
+            logger.info(response.data);
+            //console.log(response.data);
+            res.render('auctions', {
+                auctionData: response.data,
+                error: null,
+                title: 'Auctions'
+            });
 
-  
+        })
+        .catch((err) => {
+            res.render('auctions', {
+                auctionData: [],
+                error: err.response.statusText +' Not able to fetch Data! Try again Later !',
+                title: 'Auctions'
+            });
+        });
 
-  res.render('auctions', {
-    auctionData: {},
-    title: 'Auctions'
-  });
 });
+
 
 module.exports = router;
